c467ec00be57c49a5f7580b42131fea1f24e8d89
Leila Abdelrahman <58233608+leilaabdel@users.noreply.github.com>
2020-11-22 12:00:04 -0500

    Update preprocess.py

--- utils/preprocess.py
+++ utils/preprocess.py
@@ -7,7 +7,7 @@ import numpy as np
 from tqdm.notebook import tqdm
 import h5py
 
-def preprocess(data_root_path  , img_dim  , outpath , channels=1 , mode="train" , normalize=True):
+def preprocess(data_root_path  , img_dim  , outpath , num_samples = None , channels=1 , mode="train" , normalize=True):
   files = glob.glob(f"{DATA_ROOT}/Mass*/*/")
   if channels == 1:
     read = cv2.IMREAD_GRAYSCALE
@@ -19,12 +19,17 @@ def preprocess(data_root_path  , img_dim  , outpath , channels=1 , mode="train" 
   else:
     factor = 1.
 
-  for i in tqdm(range(len(files))):
+  if num_samples == None:
+    limit = len(files)
+  else:
+    limit = num_samples
+
+  for i in tqdm(range(limit)):
     if i == 0:
       all_imgs = cv2.resize(cv2.imread(glob.glob(f"{files[105]}/*/*.png")[0] , read) , (img_dim))[np.newaxis , : , :] / factor
-    else:
+    else: 
       new_img = cv2.resize(cv2.imread(glob.glob(f"{files[105]}/*/*.png")[0] ,  read) , (img_dim))[np.newaxis , : , :] / factor
-      all_imgs = np.vstack((all_imgs , new_img) )
+      all_imgs = np.vstack((all_imgs , new_img) ) 
 
   os.makedirs(os.path.dirname(outpath), exist_ok=True)
 
